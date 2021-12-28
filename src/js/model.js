d57f17c8aa9937de7dda663e46c8e67f79dded69
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-12-28 13:45:39 -0400

    Helpers and configuration files

--- src/js/model.js
+++ src/js/model.js
@@ -1,19 +1,14 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
-
   try {
-    // https://forkify-api.herokuapp.com/v2
-    const response = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const json = await response.json();
-
-    if (!response.ok) {
-      throw new Error(`${json.message} (${response.status})`);
-    }
+    const json = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = json.data;
     state.recipe = {
@@ -29,6 +24,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (error) {
-    alert(error);
+    console.error("*** " + error);
   }
 };
