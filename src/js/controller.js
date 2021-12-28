5f3457e0b34c59b74cef1d78fcd09ffbb8688bee
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-12-28 09:19:54 -0400

    Loading a recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function() {
+  try {
+    const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const json = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`${json.message} (${response.status})`);
+    }
+
+    console.log(json);
+  
+    let {recipe} = json.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      servings: recipe.servings
+    }
+
+    console.log(recipe);
+  
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+showRecipe();
